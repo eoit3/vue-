42cb3eeda235f58f1f1b015ed2ba401474a21d5f
chengquan <hxjjm@qq.com>
2019-05-30 20:59:41 +0800

    添加内容

---
+++ src/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+    created(){
+      const angular = null
+      console.time()
+      for (var i = 0; i <= 100; i++) {
+        
+      }
+      console.timeEnd()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
